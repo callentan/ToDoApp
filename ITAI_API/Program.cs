05cf5807fc9c23322fbb4fabef40dc88eb8d790f
XiZhen.Tan <xizhen.tan@siemens-healthineers.com>
2024-06-20 13:48:35 +0800

    Upload Api sample

---
+++ ITAI_API/Program.cs
@@ -0,0 +1,30 @@
+using ITAI_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = "Server=tcp:117.88.94.61,1433;Database=itai-poc;User ID=admin;Password=Pwd@1234;";
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseSqlServer(connectionString));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
