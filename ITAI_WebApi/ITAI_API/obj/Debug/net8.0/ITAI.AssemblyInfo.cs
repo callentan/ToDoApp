626550cb6c878b5d8db47deebcae21f4a89aaa05
XiZhen.Tan <xizhen.tan@siemens-healthineers.com>
2024-06-27 10:27:49 +0800

    update

--- ITAI_WebApi/ITAI_API/obj/Debug/net8.0/ITAI.AssemblyInfo.cs
+++ ITAI_WebApi/ITAI_API/obj/Debug/net8.0/ITAI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d7b121584c6138435da1394157ed569aa16df6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7393552f02d5c4a462b0c9f78d3cd77501dc435a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ITAI_WebApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ ITAI_WebApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d4f760c074f3e3e924391888951540f34f0933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7393552f02d5c4a462b0c9f78d3cd77501dc435a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
